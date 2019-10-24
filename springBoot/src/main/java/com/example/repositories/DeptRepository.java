d21e37686aeff2fcbd89997aab568f890d238c63
baekcm <baekcm@gmail.com>
2019-10-24 13:51:27 +0900

    2019.10-24 update start
    eclipse git test

--- springBoot/src/main/java/com/example/repositories/DeptRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.model.Dept;
-
-public interface DeptRepository extends JpaRepository<Dept, Integer> {
-
-}
